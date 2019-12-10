843343e836325284ad2931493224acaba925c5d6
yuwuquan <yuwuquan@lvmama.com>
2019-12-10 09:38:29 +0800

    追加

--- demo/src/test/java/com/yuwuquan/demo/common/commonTest.java
+++ demo/src/test/java/com/yuwuquan/demo/common/commonTest.java
@@ -2,9 +2,13 @@ package com.yuwuquan.demo.common;
 
 import com.yuwuquan.demo.designpatterns.specialstrategy.strategytype.IStrategy;
 import com.yuwuquan.demo.util.multithreading.ExecutorServiceFactory;
+import org.jboss.netty.util.internal.NonReentrantLock;
 
 import java.util.HashMap;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class commonTest {
     private static int num = 0;
@@ -14,20 +18,37 @@ public class commonTest {
         ConcurrentMap<String, String> map = new ConcurrentHashMap<>();
         ExecutorServiceFactory executorServiceFactory = new ExecutorServiceFactory(80, 100, 100);
         ExecutorService executorService = executorServiceFactory.getObject();
-        for(int i=0; i<50; i++){
-            num = i;
+        AtomicInteger  i = new AtomicInteger(1);
+        for( ; i.intValue()<5; i.getAndAdd(1)){
             executorService.submit(new Runnable() {
                 @Override
                 public void run() {
-                    for(int i=0;i<=num;i++){
-                        hashMap.put(1,i);
-                        concurrentHashMap.put(1,i);
+                    Lock lock = new NonReentrantLock();
+                    synchronized (this.getClass()){
+                        System.out.println("hah" + i);
+                        for(int j=0;j<=i.intValue();j++){
+                            hashMap.put(1,j);
+                            lock.lock();
+                            try{
+                                Thread.sleep(1000);
+                            }catch (Exception e){
+
+                            }
+                            concurrentHashMap.put(1,j);
+                            System.out.println("haha" + j);
+                            lock.unlock();
+                        }
                     }
+
+
+//                    for(int i=0;i<=num;i++){
+//
+//                    }
                 }
             });
+            Thread.sleep(1000);
         }
-        Thread.sleep(5000);
-        System.out.println(hashMap.get(1));//暂时没有测出来hashmap的不安全，下次测
-        System.out.println(concurrentHashMap.get(1));
+//        System.out.println(hashMap.get(1));//暂时没有测出来hashmap的不安全，下次测
+//        System.out.println(concurrentHashMap.get(1));
     }
 }
