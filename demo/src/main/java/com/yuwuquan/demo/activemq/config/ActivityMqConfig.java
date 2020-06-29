c28540acd61701340ddcefc7101f33af00038103
yuwuquan <yuwuquan@lvmama.com>
2020-06-29 14:37:06 +0800

    mq参数

--- demo/src/main/java/com/yuwuquan/demo/activemq/config/ActivityMqConfig.java
+++ demo/src/main/java/com/yuwuquan/demo/activemq/config/ActivityMqConfig.java
@@ -32,8 +32,9 @@ public class ActivityMqConfig {
 		//这里使用ActiveMQConnectionFactory连接工厂，参数调优等在这个对象里进行配置。
 		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(null, null, brokerUrl);
 		//在此进行参数调优
-		connectionFactory.getPrefetchPolicy().setQueuePrefetch(50);
+		connectionFactory.getPrefetchPolicy().setQueuePrefetch(50);//限制一次性推送(push)给单个消费者的最大消息个数。消费者则使用预取极限（prefetch limit）来设置其消息缓冲区的大小
 		connectionFactory.setOptimizeAcknowledge(true);//ack优化选项（实际上默认情况下是开启的）
+		connectionFactory.setDispatchAsync(true);//不使用 ActiveMQ服务端（也就是broker）的接受生产者消息的线程 向消费者的网络连接上 发送消息，而是用其他线程来作这件事。同步，则，直接使用ActiveMQ服务端（也就是broker）的接受生产者消息的线程 向消费者的 网络连接上 发送消息
 		connectionFactory.setOptimizeAcknowledgeTimeOut(5000);//ack信息最大发送周期(毫秒)
 		return connectionFactory;
 	}
--- demo/src/test/java/Test.java
+++ demo/src/test/java/Test.java
@@ -2,32 +2,28 @@ import com.alibaba.fastjson.JSONObject;
 import com.yuwuquan.demo.util.common.StringUtil;
 import lombok.Getter;
 
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class Test implements Runnable{
-    ThreadLocal<Integer> i = new ThreadLocal<>();
-    Integer k = 0;
-    AtomicInteger j = new AtomicInteger(1);
-    @Override
-    public void run() {
-        j.getAndIncrement();
-        try {
-            Thread.currentThread().sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println(k);
+public class Test{
+    private Test(){
+
+    }
+    private static class Single{
+        private static final Test t = new Test();
+    }
+    public static Test getTest(){
+        return Single.t;
     }
 
     public static void main(String[] args) {
-        Test t = new Test();
-        Thread t1 = new Thread(t);
-        Thread t2 = new Thread(t);
-        t1.start();
-        t2.start();
+        String addition ="{\"nsOrderId\":\"\",\"nsOrderItemId\":\"\",\"orderId\":\"\",\"orderItemId\":\"\",\"suppOrderId\":\"\"}";
+        Map map = JSONObject.parseObject(addition, Map.class);
+        Object subOrderId = map.get("nsOrderItemId");
+        String vstSupplierOrderId = String.valueOf(map.get("suppOrderId"));
+        if(subOrderId == null || vstSupplierOrderId == null){
+            return;
+        }
+        System.out.println(subOrderId + "" + vstSupplierOrderId);
     }
 }
